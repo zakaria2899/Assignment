1d5ba6c89276f9cde60db18fccc06cdf8dc81727
Baris Cankül <147693702+BarisCnkl@users.noreply.github.com>
2024-09-11 22:45:56 +0200

    Repositories...

---
+++ RepositoryContracts/ILikeRepository.cs
@@ -0,0 +1,12 @@
+﻿using Entities;
+
+namespace RepositoryContracts;
+
+public interface ILikeRepository
+{
+    Task<Like> AddAsync(Like like);
+    Task UpdateAsync(Like like);
+    Task DeleteAsync(int id);
+    Task<Like> GetSingleAsync(int id);
+    IQueryable<Like> GetMany(); 
+}
---
+++ RepositoryContracts/IcommentRepository.cs
@@ -0,0 +1,12 @@
+﻿using Entities;
+
+namespace RepositoryContracts;
+
+public interface IcommentRepository
+{
+    Task<Comment> AddAsync(Comment comment);
+        Task UpdateAsync(Comment comment);
+        Task DeleteAsync(int id);
+        Task<Comment> GetSingleAsync(int id);
+        IQueryable<Comment> GetMany(); 
+}
