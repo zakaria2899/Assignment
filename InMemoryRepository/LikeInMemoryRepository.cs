ff014bc85154474408482fce3b1134003312e8e2
Alperen941 <alperenozer03@gmail.com>
2024-09-12 19:40:09 +0200

    added 1.9

---
+++ InMemoryRepository/LikeInMemoryRepository.cs
@@ -0,0 +1,62 @@
+﻿using Entities;
+using RepositoryContracts;
+
+namespace InMemoryRepository;
+
+public class LikeInMemoryRepository : ILikeRepository
+{
+    public List<Like> Likes { get; set; }
+
+    public Task<Like> AddAsync(Like like)
+    {
+        like.id = Likes.Any() 
+            ? Likes.Max(p => p.id) + 1
+            : 1;
+        Likes.Add(like);
+        return Task.FromResult(like);
+    }
+
+    public Task UpdateAsync(Like like)
+    {
+        Like? existingLike = Likes.SingleOrDefault(p => p.id == like.id);
+        if (existingLike is null)
+        {
+            throw new InvalidOperationException(
+                $"Like with ID '{like.id}' not found");
+        }
+
+        Likes.Remove(existingLike);
+        Likes.Add(like);
+
+        return Task.CompletedTask;
+    }
+
+    public Task DeleteAsync(int id)
+    {
+        Like? likeToRemove = Likes.SingleOrDefault(p => p.id == id);
+        if (likeToRemove is null)
+        {
+            throw new InvalidOperationException(
+                $"Like with ID '{id}' not found");
+        }
+
+        Likes.Remove(likeToRemove);
+        return Task.CompletedTask;
+    }
+
+    public Task<Like> GetSingleAsync(int id)
+    {
+        Like? likeToGet = Likes.SingleOrDefault(p => p.id == id);
+        if (likeToGet is null)
+        {
+            throw new InvalidOperationException(
+                $"Like with ID '{id}' not found");
+        }
+        return Task.FromResult(likeToGet);
+    }
+
+    public IQueryable<Like> GetMany()
+    {
+        return Likes.AsQueryable();
+    }
+}
---
+++ InMemoryRepository/PostInMemoryRepository.cs
@@ -0,0 +1,62 @@
+﻿using Entities;
+using RepositoryContracts;
+
+namespace InMemoryRepository;
+
+public class PostInMemoryRepository : IpostRepository
+{
+    public List<Post> Posts { get; set; }
+
+    public Task<Post> AddAsync(Post post)
+    {
+        post.Id = Posts.Any() 
+            ? Posts.Max(p => p.Id) + 1
+            : 1;
+        Posts.Add(post);
+        return Task.FromResult(post);
+    }
+
+    public Task UpdateAsync(Post post)
+    {
+        Post? existingPost = Posts.SingleOrDefault(p => p.Id == post.Id);
+        if (existingPost is null)
+        {
+            throw new InvalidOperationException(
+                $"Post with ID '{post.Id}' not found");
+        }
+
+        Posts.Remove(existingPost);
+        Posts.Add(post);
+
+        return Task.CompletedTask;
+    }
+
+    public Task DeleteAsync(int id)
+    {
+        Post? postToRemove = Posts.SingleOrDefault(p => p.Id == id);
+        if (postToRemove is null)
+        {
+            throw new InvalidOperationException(
+                $"Post with ID '{id}' not found");
+        }
+
+        Posts.Remove(postToRemove);
+        return Task.CompletedTask;
+    }
+
+    public Task<Post> GetSingleAsync(int id)
+    {
+        Post? postToGet = Posts.SingleOrDefault(p => p.Id == id);
+        if (postToGet is null)
+        {
+            throw new InvalidOperationException(
+                $"Post with ID '{id}' not found");
+        }
+        return Task.FromResult(postToGet);
+    }
+
+    public IQueryable<Post> GetMany()
+    {
+        return Posts.AsQueryable();
+    }
+}
